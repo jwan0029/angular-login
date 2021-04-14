019013efce1fbc45e8be5400cc6bbd660b04176a
jwan0029 <angelwang14@gmail.com>
2021-04-14 16:47:19 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# angular-10-facebook-login-example
+
+Angular 10 - Facebook Login Example
+
+Tutorial and demo available at https://jasonwatmore.com/post/2020/09/21/angular-10-facebook-login-tutorial-example
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,39 @@
+﻿import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AccountService } from '@app/_services';
+
+@Component({ 
+    selector: 'app-login',
+    templateUrl: 'login.component.html' ,
+    styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+    submitted = false;
+    onSubmit(data){
+    this.submitted = true;
+    console.warn(data);
+    this.router.navigate(['/']);
+    
+}
+
+    constructor(
+        private router: Router,
+        private accountService: AccountService
+    ) {
+        // redirect to home if already logged in
+        if (this.accountService.accountValue) {
+            this.router.navigate(['/']);
+        }
+
+        if (this.submitted == true) {
+            this.router.navigate(['/']);
+        }
+    }
+
+    login() {
+        this.accountService.login();
+        this.router.navigate(['/']);
+    }
+}
